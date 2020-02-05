a3c383f620da49f8a6e42f07028b22cb1cfdf5aa
Guillaume Pitot <gpitot@nine.com.au>
2020-02-05 17:21:16 +1100

    gallery is now purely visual

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,33 +1,22 @@
-import React, { useEffect } from "react";
+import React from "react";
 import styled from "styled-components";
 import { useLoadData } from "../hooks/useLoadData";
 import { filter } from "../utils/filter";
 import Tile from "./Tile";
 import { SectionTitle } from "../shared-styles/SectionTitle";
 
-const Gallery = ({ query, url }) => {
-  const [loading, data] = useLoadData(url);
-  if (loading) return <h1>loading</h1>;
-
-  const filteredData = filter(query, data);
-
+const Gallery = ({ places }) => {
   return (
-    <GalleryArea>
-      <SectionTitle>Gallery</SectionTitle>
-
-      <TileGallery>
-        {filteredData.map(place => {
-          const { title } = place;
-
-          return <Tile key={title} {...place} />;
-        })}
-      </TileGallery>
-    </GalleryArea>
+    <TileAlbum>
+      {places.map(place => {
+        const { title } = place;
+        return <Tile key={title} {...place} />;
+      })}
+    </TileAlbum>
   );
 };
 
-const GalleryArea = styled.div``;
-const TileGallery = styled.div`
+const TileAlbum = styled.div`
   display: flex;
   flex-flow: row wrap;
 `;
