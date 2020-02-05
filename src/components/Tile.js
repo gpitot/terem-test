4725f3e8e53aa009e579ebeb33513942b25239d9
Guillaume Pitot <gpitot@nine.com.au>
2020-02-05 16:52:31 +1100

    tile added

---
+++ src/components/Tile.js
@@ -0,0 +1,27 @@
+import React from "react";
+import styled from "styled-components";
+
+const Tile = ({ title, img, location }) => (
+  <TileArea>
+    <Image src={img} />
+    <Title>{title}</Title>
+    <Location>{location}</Location>
+  </TileArea>
+);
+
+const TileArea = styled.div`
+  flex-basis: 300px;
+  flex-shrink: 1;
+  flex-grow: 0;
+  margin:10px;
+`;
+const Image = styled.img`
+    display:block;
+    width:100%;
+    height:300px;
+    object-fit:cover;
+`;
+const Title = styled.h4``;
+const Location = styled.p``;
+
+export default Tile;
