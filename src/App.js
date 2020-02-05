6fcc8366ec5d238d32fff272dd5f5813414d3b43
Guillaume Pitot <gpitot@nine.com.au>
2020-02-05 17:05:34 +1100

    added search bar and gallery

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, { useState } from "react";
+import styled, { ThemeProvider } from "styled-components";
+import Gallery from "./components/Gallery";
+import SearchBar from "./components/SearchBar";
 
+const DEFAULT_THEMES = {
+  light: {
+    background: "#fff"
+  },
+  dark: {}
+};
 
-function App() {
+const App = () => {
+  const [theme, setTheme] = useState(DEFAULT_THEMES.light);
+
+  const [query, setQuery] = useState("");
+
   return (
-    <div className="App">
-     
-    </div>
+    <ThemeProvider theme={theme}>
+      <Article>
+        <SearchBar query={query} setQuery={setQuery} />
+
+        <Gallery
+          url={"http://demo3136867.mockable.io/featured"}
+          query={query}
+        />
+      </Article>
+    </ThemeProvider>
   );
-}
+};
+
+const Article = styled.article`
+  width: 100%;
+  max-width: 1200px;
+  margin: 50px auto;
+  background: ${props => props.theme.background};
+`;
 
 export default App;
