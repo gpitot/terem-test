55d70dce8f4e2955dcc62e60aca7644cda4f9cee
Guillaume Pitot <gpitot@nine.com.au>
2020-02-06 10:37:40 +1100

    tile loading image

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,10 +1,21 @@
-import React from "react";
-import styled from "styled-components";
+import React, { useState, useEffect } from "react";
+import styled, { keyframes } from "styled-components";
 
 const Tile = ({ visible, title, img, location }) => {
+  const [loadingImage, setLoadingImage] = useState(true);
+
+  useEffect(() => {
+    const imagePreload = new Image();
+    imagePreload.onload = () => {
+      setLoadingImage(false);
+    };
+    imagePreload.src = img;
+  }, []);
+
   return (
     <TileArea visible={visible}>
-      <Image src={img} />
+      {loadingImage ? <LoadingBlock as="div" /> : <ImageBlock src={img} />}
+
       <Title>{title}</Title>
       <Location>{location}</Location>
     </TileArea>
@@ -17,11 +28,29 @@ const TileArea = styled.div`
   margin: 10px;
   display: ${props => (props.visible ? "block" : "none")};
 `;
-const Image = styled.img`
+
+const FadeIn = keyframes`
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+`;
+
+const ImageBlock = styled.img`
   display: block;
   width: 100%;
   height: 300px;
   object-fit: cover;
+  opacity: 0;
+  animation-name: ${FadeIn};
+  animation-delay: 50ms;
+  animation-duration: 500ms;
+  animation-fill-mode: forwards;
+`;
+const LoadingBlock = styled(ImageBlock)`
+  background: gray;
 `;
 const Title = styled.h4``;
 const Location = styled.p``;
