3435103e2243234f699577a6a0730e88a7ce2360
Guillaume Pitot <gpitot@nine.com.au>
2020-02-06 15:26:22 +1100

    build 06-02-2020

---
+++ precache-manifest.e139e7653ea6ae9f2905205ee089cb31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "438ad7838f8dee7194f21b0fa95f8a15",
+    "url": "/terem-test/index.html"
+  },
+  {
+    "revision": "ec4ad3fcc8f6fcc38e86",
+    "url": "/terem-test/static/css/main.fa136c5f.chunk.css"
+  },
+  {
+    "revision": "028b4e4febac55bc0901",
+    "url": "/terem-test/static/js/2.c50976ae.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/terem-test/static/js/2.c50976ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec4ad3fcc8f6fcc38e86",
+    "url": "/terem-test/static/js/main.c2156b70.chunk.js"
+  },
+  {
+    "revision": "3bd4656772767a900fc4",
+    "url": "/terem-test/static/js/runtime-main.69278a6a.js"
+  }
+]);
