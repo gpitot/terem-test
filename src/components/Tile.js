860a0e07f34d7960222ffd13b55a29f8c75d7b24
Guillaume Pitot <gpitot@nine.com.au>
2020-02-06 12:49:26 +1100

    hover effect on tile

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -14,20 +14,20 @@ const Tile = ({ visible, title, img, location }) => {
 
   return (
     <TileArea visible={visible}>
-      {loadingImage ? <LoadingBlock as="div" /> : <ImageBlock src={img} />}
-
-      <Title>{title}</Title>
-      <Location>{location}</Location>
+      {loadingImage ? (
+        <LoadingBlock />
+      ) : (
+        <ImageOuter>
+          <ImageBlock src={img} />
+        </ImageOuter>
+      )}
+      <Text>
+        <Title>{title}</Title>
+        <Location>{location}</Location>
+      </Text>
     </TileArea>
   );
 };
-const TileArea = styled.div`
-  flex-basis: 300px;
-  flex-shrink: 0;
-  flex-grow: 0;
-  margin: 10px;
-  display: ${props => (props.visible ? "block" : "none")};
-`;
 
 const FadeIn = keyframes`
   from {
@@ -38,21 +38,55 @@ const FadeIn = keyframes`
   }
 `;
 
+const ImageOuter = styled.div`
+  width: 100%;
+  height: 300px;
+  overflow: hidden;
+`;
+
 const ImageBlock = styled.img`
   display: block;
   width: 100%;
-  height: 300px;
+  height: 100%;
   object-fit: cover;
   opacity: 0;
   animation-name: ${FadeIn};
-  animation-delay: 50ms;
+  animation-delay: 750ms;
   animation-duration: 500ms;
   animation-fill-mode: forwards;
+  transition: transform 300ms ease;
 `;
-const LoadingBlock = styled(ImageBlock)`
-  background: gray;
+const LoadingBlock = styled(ImageOuter)``;
+
+const Text = styled.div`
+  padding: 20px;
 `;
-const Title = styled.h4``;
-const Location = styled.p``;
+
+const Title = styled.h4`
+  color: #2d91b2;
+  margin: 0;
+  font-size: 14px;
+`;
+const Location = styled.p`
+  font-size: 12px;
+  margin-bottom: 0;
+  color: #851717;
+`;
+
+const TileArea = styled.div`
+  flex-basis: 300px;
+  flex-shrink: 0;
+  flex-grow: 0;
+  margin: 10px;
+  display: ${props => (props.visible ? "block" : "none")};
+  background: #eee;
+  cursor: pointer;
+
+  &:hover {
+    ${ImageBlock} {
+      transform: scale(1.1);
+    }
+  }
+`;
 
 export default Tile;
